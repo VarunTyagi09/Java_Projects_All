68d3e949a7b6183adbda167c846dbbb2dfc22a22
Varun Tyagi <varuntyagiben@gmail.com>
2026-02-10 22:12:06 +0530

    Auto update

--- Marks_Grade_Evaluation.java
+++ Marks_Grade_Evaluation.java
@@ -18,30 +18,31 @@ public class Marks_Grade_Evaluation {
             System.out.print(" Invalid! Enter Correct marks again (0-100): ");
 
             while (true) {
-            if (Grade >= 90 && Grade <= 100) {
-                System.out.print("Your Grades: A+");
+                if (Grade >= 90 && Grade <= 100) {
+                    System.out.print("Your Grades: A+");
 
 
-            } else if (Grade >= 80 && Grade <= 89) {
-                System.out.print("Your Grades: A ");
+                } else if (Grade >= 80 && Grade <= 89) {
+                    System.out.print("Your Grades: A ");
 
-            } else if (Grade >= 70 && Grade <= 79) {
-                System.out.print("Your Grades: B+ ");
+                } else if (Grade >= 70 && Grade <= 79) {
+                    System.out.print("Your Grades: B+ ");
 
-            } else if (Grade >= 60 && Grade <= 69) {
-                System.out.print("Your Grades: B ");
+                } else if (Grade >= 60 && Grade <= 69) {
+                    System.out.print("Your Grades: B ");
 
-            } else if (Grade >= 50 && Grade <= 59) {
-                System.out.print("Your Grades: C ");
+                } else if (Grade >= 50 && Grade <= 59) {
+                    System.out.print("Your Grades: C ");
 
-            } else if (Grade >= 45 && Grade <= 49) {
-                System.out.print("Your Grades: D ");
+                } else if (Grade >= 45 && Grade <= 49) {
+                    System.out.print("Your Grades: D ");
 
-            } else if (Grade >= 40 && Grade <= 44) {
-                System.out.print("Your Grades: E ");
+                } else if (Grade >= 40 && Grade <= 44) {
+                    System.out.print("Your Grades: E ");
 
+                }
+                input.close();
             }
-            input.close();
         }
     }
 }
