0eaa90b7b520add0e03a1529c2aba01073e25956
Varun Tyagi <varuntyagiben@gmail.com>
2026-02-10 22:08:23 +0530

    Auto update

--- Marks_Grade_Evaluation.java
+++ Marks_Grade_Evaluation.java
@@ -14,6 +14,9 @@ public class Marks_Grade_Evaluation {
         System.out.print("Enter Your Grades: ");
         while (true) {
             float Grade = input.nextFloat();
+            if (Grade >= 0 && Grade <= 100) break;
+            System.out.print(" Invalid! Enter Correct marks again (0-100): ");
+
 
             if (Grade >= 90 && Grade <= 100) {
                 System.out.print("Your Grades: A+");
@@ -38,8 +41,6 @@ public class Marks_Grade_Evaluation {
                 System.out.print("Your Grades: E ");
 
             }
-            if (Grade >= 0 && Grade <= 100) break;
-            System.out.print(" Invalid! Enter Correct marks again (0-100): ");
             input.close();
         }
     }
